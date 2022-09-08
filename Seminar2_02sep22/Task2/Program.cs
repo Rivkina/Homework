7b30de3ca3ecb8bb66c63fc8fdd6db59a1c97a50
Rivkina <rivkinathebest@gmail.com>
2022-09-09 02:23:50 +0300

    Добавила ДЗ2

---
+++ Seminar2_02sep22/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+Console.Write("Введите число: ");
+long x = Convert.ToInt64(Console.ReadLine());
+
+int y = 10;
+int count = 1;
+
+if (x < 100 && x > -100)
+{
+    Console.Write("Третьей цифры нет");
+}
+
+else
+{
+    while (x / y > 99 || x / y < -99)
+    {
+        y = y * 10;
+        count = count * 10;
+    }
+
+    long result = (x % y) / count;
+    Console.Write($"Третья цифра: {Math.Abs(result)}");
+}
---
+++ Seminar2_02sep22/Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите цифру соответствующую дню недели от 1 до 7: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+switch(x)
+{
+    case 1:
+    System.Console.WriteLine("Понедельник - рабочий день");
+    break;
+
+    case 2:
+    System.Console.WriteLine("Вторник - рабочий день");
+    break;
+
+    case 3:
+    System.Console.WriteLine("Среда - рабочий день");
+    break;
+
+    case 4:
+    System.Console.WriteLine("Четверг - рабочий день");
+    break;
+
+    case 5:
+    System.Console.WriteLine("Пянтица - рабочий день");
+    break;
+
+    case 6:
+    System.Console.WriteLine("Суббота - выходной день");
+    break;
+
+    case 7:
+    System.Console.WriteLine("Воскресенье - выходной день");
+    break;
+
+    default:
+    System.Console.WriteLine("Это не день недели");
+    break;
+
+}
